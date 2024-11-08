118bf78de19f69a7dc32132332bd95023fe93d2f
nashitnoor <nshtnoor15@gmail.com>
2024-11-09 02:06:44 +0530

    added new endpoint to product controller

--- sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -3,14 +3,15 @@ package com.ecommerce.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
 import com.ecommerce.project.service.ProductService;
@@ -23,8 +24,8 @@ public class ProductController {
 	private ProductService productService;
 	
 	@PostMapping("/admin/categories/{categoryId}/product")
-	public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product, @PathVariable Long categoryId){
-		return new ResponseEntity<ProductDTO>(productService.addProduct(categoryId, product), HttpStatus.CREATED);
+	public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO, @PathVariable Long categoryId){
+		return new ResponseEntity<ProductDTO>(productService.addProduct(categoryId, productDTO), HttpStatus.CREATED);
 	}
 	
 	@GetMapping("/public/products")
@@ -36,5 +37,23 @@ public class ProductController {
 	public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId){
 		ProductResponse productResponse = productService.searchByCategory(categoryId);
 		return new ResponseEntity<>(productResponse, HttpStatus.OK);
+	}
+	
+	@GetMapping("/public/products/keyword/{keyword}")
+	public ResponseEntity<ProductResponse> getProductsByKeyword(@PathVariable String keyword){
+		ProductResponse productResponse = productService.searchProductByKeyword(keyword);
+		return new ResponseEntity<>(productResponse, HttpStatus.OK);
+	}
+	
+	@PutMapping("/admin/products/{productid}")
+	public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO productDTO, @PathVariable Long productId){
+		ProductDTO updatedProductDTO = productService.updateProduct(productDTO, productId);
+		return new ResponseEntity<>(updatedProductDTO, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/admin/products/{productId}")
+	public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId){
+		ProductDTO deletedProduct = productService.deleteProduct(productId);
+		return new ResponseEntity<>(deletedProduct, HttpStatus.OK);
 	}
 }
--- sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -27,10 +27,11 @@ public class ProductServiceImpl implements ProductService {
 	private ModelMapper modelMapper;
 	
 	@Override
-	public ProductDTO addProduct(Long categoryId, Product product) {
+	public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
 		Category category = categoryRepository
 				.findById(categoryId)
 				.orElseThrow(()-> new ResourceNotFoundException("Category", "categoryId", categoryId));
+		Product product = modelMapper.map(productDTO, Product.class);
 		product.setImage("default.png");
 		product.setCategory(category);
 		product.setSpecialPrice(product.getPrice() - ((product.getDiscount()*0.01)*product.getPrice()));
@@ -59,6 +60,38 @@ public class ProductServiceImpl implements ProductService {
 		ProductResponse productResponse = new ProductResponse();
 		productResponse.setContent(productDTOS);
 		return productResponse;
+	}
+
+	@Override
+	public ProductResponse searchProductByKeyword(String keyword) {
+		List<Product> products = productRepository.findByProductNameLikeIgnoreCase('%'+keyword+'%');
+		List<ProductDTO> productDTOS = products.stream().map(product -> modelMapper.map(product, ProductDTO.class)).toList();
+		
+		ProductResponse productResponse = new ProductResponse();
+		productResponse.setContent(productDTOS);
+		return productResponse;
+	}
+
+	@Override
+	public ProductDTO updateProduct(ProductDTO productDTO, Long productId) {
+		Product productFromDb = productRepository.findById(productId).orElseThrow(()-> new ResourceNotFoundException("Product", "ProductId", productId));
+		Product product = modelMapper.map(productDTO, Product.class);
+		productFromDb.setProductName(product.getProductName());
+		productFromDb.setDescription(product.getDescription());
+		productFromDb.setQuantity(product.getQuantity());
+		productFromDb.setDiscount(product.getDiscount());
+		productFromDb.setSpecialPrice(product.getSpecialPrice());
+		
+		Product savedProduct = productRepository.save(productFromDb);
+		
+		return modelMapper.map(savedProduct, ProductDTO.class);
+	}
+
+	@Override
+	public ProductDTO deleteProduct(Long productId) {
+		Product productFromDb = productRepository.findById(productId).orElseThrow(()-> new ResourceNotFoundException("Product", "ProductId", productId));
+		productRepository.delete(productFromDb);
+		return modelMapper.map(productFromDb, ProductDTO.class);
 	}
 
 }
