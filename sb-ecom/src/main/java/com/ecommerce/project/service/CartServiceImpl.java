9f19ed8634c40e85c975ac912072a5a712c306a1
nashitnoor <nshtnoor15@gmail.com>
2025-01-15 17:30:51 +0530

    final changes done

--- sb-ecom/src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -118,7 +118,11 @@ public class CartServiceImpl implements CartService {
 			CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
 			
 			List<ProductDTO> products = cart.getCartItems().stream()
-					.map(p -> modelMapper.map(p.getProduct(), ProductDTO.class))
+					.map(cartItem -> {
+						ProductDTO productDTO = modelMapper.map(cartItem.getProduct(), ProductDTO.class);
+						productDTO.setQuantity(cartItem.getQuantity());
+						return productDTO;
+					})
 					.collect(Collectors.toList());
 			
 			cartDTO.setProducts(products);
